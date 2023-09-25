877b80ee4f7eb580e6fc2f5cfe12387632de7f70
DevExpressExampleBot <scmaintenance@devexpress.com>
2023-09-25 17:20:59 +0400

    Change example status to out of maintenance

--- VB/T300213wf/App_Code/Store.vb
+++
@@ -1,9 +0,0 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-
-Public Class Store
-    Public Property Id() As Integer
-    Public Property Name() As String
-End Class
